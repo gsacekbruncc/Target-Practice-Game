bb87f1af820420d419415b53974e09b6a88935a7
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2025-07-15 19:22:19 -0400

    Add MoveCharacter script
    - WASD movement
    - Jump on Space key

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float jumpForce = 10;
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = new Vector3(x, 0, z) * moveSpeed * Time.deltaTime;
+
+        transform.Translate(move, Space.Self);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
