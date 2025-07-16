e70066a6d9d81f03ca16593d1c0c506a5a520b3f
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2025-07-16 19:25:38 -0400

    Add ShootTarget script and Target tag #3
    - Introduced the ShootTarget script to handle shooting mechanics using raycasting.
    - Added the 'Target' tag in TagManager to identify objects that can be destroyed when hit.

---
+++ Assets/Scripts/ShootTarget.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTarget : MonoBehaviour
+{
+
+    public float rayDistance = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.red, 100);
+
+        if(Physics.Raycast(ray, out RaycastHit hit, rayDistance))
+        {
+            if(hit.collider.gameObject.CompareTag("Target"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+    
+}
