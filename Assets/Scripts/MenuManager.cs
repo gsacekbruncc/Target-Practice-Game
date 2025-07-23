b00a0536597473cab95472be08ff7cd5b1ec1510
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2025-07-23 19:54:04 -0400

    Add quit functionality and resize game mode UI
    - Updated MenuManager to add functionality to the Quit button
    - Decreased the size and spread of the game mode buttons

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -9,6 +9,7 @@ public class MenuManager : MonoBehaviour
     GameObject button;
     
     public GameObject[] buttons;
+    public GameObject quitButton;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +23,8 @@ public class MenuManager : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = new Ray(cam.transform.position, cam.transform.forward);
-
-            if(Physics.Raycast(ray, out var hit) && buttons.Contains(hit.collider.gameObject))
+            var hitInfo = Physics.Raycast(ray, out var hit);
+            if(hitInfo && buttons.Contains(hit.collider.gameObject))
             {   
                 var currButton = hit.collider.gameObject;
                 GameObject currSubMenu = currButton.GetComponent<ClickButton>().subMenu;
@@ -46,6 +47,20 @@ public class MenuManager : MonoBehaviour
                 {
                     buttons[1].GetComponent<ClickButton>().subMenu.SetActive(false);
                 }
+
+                if(currId == 3)
+                {
+                    Application.Quit();
+                }
+            }
+            else if(hitInfo && hit.collider.gameObject == quitButton)
+            {   
+                #if UNITY_STANDALONE
+                    Application.Quit();
+                #endif
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #endif
             }
         }
     }
