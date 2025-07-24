b211661f1fc5f50bb1fa566bd5d079f14c9e5a37
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2025-07-23 23:47:27 -0400

    Add tutorial level
    - Added GameModeManager script to handle game mode selection from the world space menu
    - Updated SpawnTarget script to start handle tutorial level sequencing

--- Assets/Scripts/SpawnTarget.cs
+++ Assets/Scripts/SpawnTarget.cs
@@ -10,26 +10,57 @@ public class SpawnTarget : MonoBehaviour
     public float roundTime;
     public float liveTime;
     public GameObject target;
+    public GameObject[] tutorialTargets;
 
+    bool inTutorial;
+    int tutorialStage;
+    bool inRound;
 
     // Start is called before the first frame update
     void Start()
     {
+        inTutorial = false;
+        inRound = false;
         roundTime += startDelay;
-        InvokeRepeating(nameof(Spawn), startDelay, interval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(roundTime > 0)
+
+        if(inTutorial)
         {
-            roundTime -= Time.deltaTime;
+            if(tutorialStage == 0 && tutorialTargets[0].activeInHierarchy == false && tutorialTargets[1].activeInHierarchy == false && tutorialTargets[2].activeInHierarchy == false)
+            {
+                tutorialTargets[0].SetActive(true);
+                tutorialStage = 1;             
+            }
+            if(tutorialStage == 1 && tutorialTargets[0].activeInHierarchy == false && tutorialTargets[2].activeInHierarchy == false)
+            {
+                tutorialTargets[1].SetActive(true);
+                tutorialStage = 2;
+            }
+            if(tutorialStage == 2 && tutorialTargets[1].activeInHierarchy == false && tutorialTargets[0].activeInHierarchy == false)
+            {
+                tutorialTargets[2].SetActive(true);
+                tutorialStage = 3;
+            }
+            if(tutorialStage == 3)
+            {
+                inTutorial = false;
+            }
         }
-        else
-        {  
-            //CancelInvoke() stops all invokes
-            CancelInvoke(nameof(Spawn));
+        else if(inRound)
+        {
+            if(roundTime > 0)
+            {
+                roundTime -= Time.deltaTime;
+            }
+            else
+            {  
+                //CancelInvoke() stops all invokes
+                CancelInvoke(nameof(Spawn));
+            }
         }
     }
 
@@ -55,8 +86,17 @@ public class SpawnTarget : MonoBehaviour
         {
             var st = newTarget.AddComponent<ShootPlayer>();
             st.time = 5;
-        }
-        
-        
+        }  
+    }
+
+    void StartRound()
+    {
+        InvokeRepeating(nameof(Spawn), startDelay, interval);
+        inRound = true;
+    }
+
+    public void StartTutorial()
+    {
+        inTutorial = true;
     }
 }
