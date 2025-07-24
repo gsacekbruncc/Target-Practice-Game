0910849acef08e818cd3d49ab3eb890f0da06580
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2025-07-23 23:41:35 -0400

    Update MoveTarget script
    - Updated MoveTarget script to allow manually setting the direction of movement

--- Assets/Scripts/MoveTarget.cs
+++ Assets/Scripts/MoveTarget.cs
@@ -6,9 +6,11 @@ public class MoveTarget : MonoBehaviour
 {
     public float dx;
     public float speed;
+    public int presetDirection;
 
     Vector3 origin;
     int direction;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +22,29 @@ public class MoveTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(direction == 0)
+        if(presetDirection == 0)
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(origin.x + dx, origin.y, origin.z), speed * Time.deltaTime);
+            if(direction == 0)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(origin.x + dx, origin.y, origin.z), speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(origin.x, origin.y + dx, origin.z), speed * Time.deltaTime);        
+            }
         }
         else
         {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(origin.x, origin.y + dx, origin.z), speed * Time.deltaTime);        
+            if(presetDirection == -1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(origin.x + dx, origin.y, origin.z), speed * Time.deltaTime);
+            }
+            if(presetDirection == 1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(origin.x, origin.y + dx, origin.z), speed * Time.deltaTime);
+            }
         }
+        
         if(transform.position == new Vector3(origin.x + dx, origin.y, origin.z) 
         || transform.position == new Vector3(origin.x - dx, origin.y, origin.z) 
         || transform.position == new Vector3(origin.x, origin.y + dx, origin.z) 
