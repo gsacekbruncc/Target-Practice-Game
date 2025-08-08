cf7711b675bed929e0ecf3cd372cc5ab8d3b1bc1
Gavin Sacek-Brooks <gsacekbr@charlotte.edu>
2025-08-07 22:50:32 -0400

    Add KillPlayer script
    - Added KillPlayer script which calls LevelManager.CancelGameMode() on collision with the player

---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    LevelManager LevelManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelManager = GameObject.Find("Game Handler").GetComponent<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if(c.collider.CompareTag("Player"))
+        {
+            LevelManager.CancelGameMode();    
+        }  
+    }
+}
